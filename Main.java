33dd2c53bfb59d866cbba4652d8fdbf37aa65ee2
2400415 <162500595+2400415@users.noreply.github.com>
2024-03-21 18:16:51 +0200

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,8 @@
+public class Main{
+    public static void main(String[]args){
+        int a=110;
+        int b=220;
+        int c=a-b;
+        if(c>0){System.out.println("The difference is posetive !!");}else if(c<0){System.out.println("The difference is negative");}
+    }
+}
