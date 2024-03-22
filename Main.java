c49c91dcd78fd139f91da96605e720f469af94d1
2400415 <162500595+2400415@users.noreply.github.com>
2024-03-22 18:37:21 +0200

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,8 +1,15 @@
+/*project will give you reminder of any two numbers you enter*/
+import java.util.Scanner;
 public class Main{
     public static void main(String[]args){
-        int a=110;
-        int b=220;
-        int c=a-b;
-        if(c>0){System.out.println("The difference is posetive !!");}else if(c<0){System.out.println("The difference is negative");}
+        Scanner input=new Scanner(System.in);
+        System.out.println("Enter your first_number:");
+        int first_number=input.nextInt();
+        System.out.println("Enter your second_number:");
+        int second_number=input.nextInt();
+        int reminder_total=first_number%second_number;
+        System.out.println("the reminder is:"+reminder_total);
+        input.close();
+
     }
 }
