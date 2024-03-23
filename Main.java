c12a0f4792031eedd15e2f158b9d2742d4e6dbe4
Regoemetse Selemala <162500595+2400415@users.noreply.github.com>
2024-03-23 13:54:17 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -8,12 +8,7 @@ public class Main {
         int second_integer = input.nextInt();
         int integer_difference = first_integer - second_integer;
         System.out.println("The answer difference:"+integer_difference);
-        /*to determine if the difference is even or odd*/
-        if (integer_difference % 2 == 0) {
-            System.out.println("which is an even number!!");
-        } else if (integer_difference % 2 >= 1) {
-            System.out.println("which is an odd number!!");}
-    }
+  }
 }
 
 
