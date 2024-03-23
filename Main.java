55798fc9ff67b688e12ac4298033f219c7ec90e2
Regoemetse Selemala <162500595+2400415@users.noreply.github.com>
2024-03-23 13:45:50 +0200

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,19 +1,8 @@
 import java.util.Scanner;
-public class Main {
-    public static void main(String[] args) {
+public class Main{
+    public static void main(String[]args) {
         Scanner input = new Scanner(System.in);
-        System.out.println("Enter your first_integer:");
-        int first_integer = input.nextInt();
-        System.out.println("Enter your second_integer:");
-        int second_integer = input.nextInt();
-        int integer_difference = first_integer - second_integer;
-        System.out.println("The answer difference:"+integer_difference);
-        /*to determine if the difference is even or odd*/
-        if (integer_difference % 2 == 0) {
-            System.out.println("which is an even number!!");
-        } else if (integer_difference % 2 >= 1) {
-            System.out.println("which is an odd number!!");}
+        System.out.println("Enter your first_number:");
+        int f = input.next.Int();
     }
 }
-
-
